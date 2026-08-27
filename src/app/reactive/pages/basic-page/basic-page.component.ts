6bc57715a4056b5783046d4594d46014eb153f10
“Lara” <lararodriguezarmada@gmail.com>
2026-08-28 00:22:37 +0200

    Commit Inicial

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,11 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [
+    JsonPipe
+  ],
+  templateUrl: './basic-page.component.html',
+})
+export class BasicPageComponent {}
---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,11 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  imports: [
+    JsonPipe
+  ],
+  templateUrl: './dynamic-page.component.html',
+})
+export class DynamicPageComponent {}
