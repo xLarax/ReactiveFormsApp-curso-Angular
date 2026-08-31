16f1b93e97fcb5ebdd6bcf143d3f9edb96a6a2db
“Lara” <lararodriguezarmada@gmail.com>
2026-08-31 22:31:07 +0200

    FormBuilder, FormGroup y Validaciones

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,27 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
-    JsonPipe
+    JsonPipe, ReactiveFormsModule
   ],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)], ],
+    price: [0, [Validators.required, Validators.min(10)],],
+    inStorage: [0, [Validators.required, Validators.min(0)],],
+  })
+
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+}
