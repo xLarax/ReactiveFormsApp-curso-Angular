947a67eddbbd82ed2f2099ab8a30905bb50af4d2
“Lara” <lararodriguezarmada@gmail.com>
2026-09-20 19:40:10 +0200

    Formulario Dinámico

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,11 +1,55 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-dynamic-page',
   imports: [
-    JsonPipe
+    JsonPipe, ReactiveFormsModule
   ],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent {}
+export class DynamicPageComponent {
+
+  private fb = inject(FormBuilder);
+
+   formUtils= FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Portal', Validators.required],
+    ],
+    Validators.minLength(3)
+  ),
+  });
+
+  newFavorite = new FormControl('', Validators.required)
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onAddToFavorites(){
+    if (this.newFavorite.invalid) return;
+
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(this.fb.control( newGame, Validators.required))
+
+    this.newFavorite.reset();
+
+  }
+
+  onDeleteFavorite(index: number){
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
+
+
+}
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,6 +1,26 @@
-import { FormGroup } from "@angular/forms";
+import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
 export class FormUtils {
+
+
+  static getTextError(errors: ValidationErrors){
+
+      for (const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es obligatorio'
+
+        case 'minlength':
+          return `Este campo debe contener al menos ${ errors['minlength'].requiredLength } caracteres`
+
+        case 'min':
+          return `El valor mínimo es de ${ errors['min'].min }`
+      }
+    }
+
+    return null;
+
+  }
 
   static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return (
@@ -15,21 +35,29 @@ export class FormUtils {
 
   const errors = form.controls[fieldName].errors ?? {} ;
 
-  for (const key of Object.keys(errors)){
-    switch(key){
-      case 'required':
-        return 'Este campo es obligatorio'
-
-      case 'minlength':
-        return `Este campo debe contener al menos ${ errors['minlength'].requiredLength } caracteres`
-
-      case 'min':
-        return `El valor mínimo es de ${ errors['min'].min }`
-    }
-  }
-
-  return null;
+  return FormUtils.getTextError(errors);
 
   }
+
+
+  //Arrays
+   static isValidFieldInArray( formArray: FormArray, index: number){
+
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+
+  }
+
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+
+  if (formArray.controls.length === 0) return null;
+
+  const errors = formArray.controls[index].errors ?? {} ;
+
+  return FormUtils.getTextError(errors);
+
+  }
+
 
 }
